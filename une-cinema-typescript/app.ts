5e491b80955cd9e2601d67607417bc60458b3a7b
Le Kang <le.kang@lendi.com.au>
2022-07-02 15:32:02 +1000

    Add Seat, Row, SeatMap classes and use tsconfig

--- une-cinema-typescript/app.ts
+++ une-cinema-typescript/app.ts
@@ -1,1 +1,54 @@
-console.log('hello, AWP')
+enum STATUS {
+  AVAILABLE = 'AVAILABLE',
+  OCCUPIED = 'OCCUPIED',
+  SELECTED = 'SELECTED',
+}
+
+class Seat {
+  id: number
+  status: STATUS
+  element: HTMLDivElement
+
+  constructor(id: number, isOccupied: boolean = false) {
+    this.id = id
+    this.status = isOccupied ? STATUS.OCCUPIED : STATUS.AVAILABLE
+    this.element = document.createElement('div')
+    this.element.classList.add('seat')
+    this.element.classList.add(this.status.toLowerCase())
+  }
+}
+
+class Row {
+  id: number
+  seats: Seat[]
+  element: HTMLDivElement
+
+  constructor(id: number, seatNumber: number) {
+    this.id = id
+    this.seats = Array.from({ length: seatNumber }).map((_, index) => {
+      const seatId = seatNumber * id + index
+      return new Seat(seatId)
+    })
+    this.element = document.createElement('div')
+    this.element.classList.add('row')
+    this.element.append(...this.seats.map((seat) => seat.element))
+  }
+}
+
+class SeatMap {
+  rows: Row[]
+  selectedSeats: number[] = []
+  element: HTMLDivElement
+
+  constructor(rowNumber: number, seatNumberPerRow: number) {
+    this.rows = Array.from({ length: rowNumber }).map((_, index) => {
+      return new Row(index, seatNumberPerRow)
+    })
+    this.element = document.createElement('div')
+    this.element.classList.add('seat-map')
+    this.element.append(...this.rows.map((row) => row.element))
+  }
+}
+
+const seatMap = new SeatMap(8, 10)
+document.getElementById('theater')?.appendChild(seatMap.element)
