24b00e9987b4ae5f738fc0a77d6f87f4d7bc380f
Le Kang <le.kang@lendi.com.au>
2022-07-08 19:14:48 +1000

    Render a list of movies

--- une-cinema-react/src/App.tsx
+++ une-cinema-react/src/App.tsx
@@ -1,7 +1,22 @@
 import './App.css'
+import movies from './data/movies.json'
 
 function App() {
-  return <></>
+  return (
+    <>
+      <header className="header">UNE Cinema</header>
+      <main className="main">
+        <div className="container">
+          {movies.map((movie) => (
+            <div className="movie" key={movie.id}>
+              <img className="poster" src={movie.poster} alt={movie.title} />
+              <div className="title">{movie.title}</div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </>
+  )
 }
 
 export default App
