38d42a70bf57f7b249307c08747d5be96fd44049
Le Kang <le.kang@lendi.com.au>
2022-07-13 14:23:30 +1000

    Setup routing

--- une-cinema-react/src/App.tsx
+++ une-cinema-react/src/App.tsx
@@ -1,5 +1,6 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { Header } from './components'
-import { Home, Login, SignUp } from './pages'
+import { Home, Login, SignUp, Movie, Session, Bookings } from './pages'
 
 import style from './App.module.css'
 
@@ -8,9 +9,15 @@ function App() {
     <>
       <Header />
       <main className={style.main}>
-        <Home />
-        {/* <Login /> */}
-        {/* <SignUp /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="movie/:movieId" element={<Movie />} />
+          <Route path="session/:sessionId" element={<Session />} />
+          <Route path="login" element={<Login />} />
+          <Route path="sign-up" element={<SignUp />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
       </main>
     </>
   )
--- une-cinema-react/src/components/Header.tsx
+++ une-cinema-react/src/components/Header.tsx
@@ -1,12 +1,17 @@
+import { Link, useNavigate } from 'react-router-dom'
 import style from './Header.module.css'
 
 export default function Header() {
+  const navigate = useNavigate()
+
   return (
     <header className={style.header}>
       <div className={style.container}>
-        <span>UNE Cinema</span>
+        <Link to="/">UNE Cinema</Link>
         <div className={style.actions}>
-          <button className={style.action}>Login</button>
+          <button className={style.action} onClick={() => navigate('login')}>
+            Login
+          </button>
         </div>
       </div>
     </header>
