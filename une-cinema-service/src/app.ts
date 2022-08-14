e7c4d8da79f9442a8ebf41586ed71eaffafddca0
Yihan Lu <yihanlu@Yihans-iMac.local>
2022-08-14 13:16:04 +1000

    connect backend with mongodb

--- une-cinema-service/src/app.ts
+++ une-cinema-service/src/app.ts
@@ -1,11 +1,16 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
+import connectDB from './util/connectDB';
 import theatreHandler from './handler/theatres.handler';
 import movieHandler from './handler/movie.handler';
 import bookingHandler from './handler/booking.handler';
 
 dotenv.config();
+
+// connect to database
+connectDB();
 
 const app: Express = express();
 const port = process.env.PORT;
@@ -15,6 +20,10 @@ app.use('/theatres', theatreHandler)
 app.use('/movies', movieHandler);
 app.use('/bookings', bookingHandler)
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
+// only listen to request when DB connection is established
+mongoose.connection.once('connected', () => {
+  console.log('⚡️[server]: Connected to MongoDB.');
+  app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+  });
+})
