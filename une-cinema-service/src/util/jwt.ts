4594dab43ab54f27e1d8b644a084a49bbc5a8d56
Yihan Lu <yihanlu@Yihans-iMac.localdomain>
2022-08-27 14:39:24 +1000

    add jwt util

---
+++ une-cinema-service/src/util/jwt.ts
@@ -0,0 +1,19 @@
+import jwt, { SignOptions } from 'jsonwebtoken';
+
+export const signJwt = (payload: Object, options: SignOptions = {}) => {
+  const privateKey = process.env.accessTokenPrivateKey as string;
+  return jwt.sign(payload, privateKey, {
+    ...(options && options),
+    algorithm: 'RS256',
+    expiresIn: '8h'
+  });
+};
+
+export const verifyJwt = <T>(token: string): T | null => {
+  try {
+    const publicKey = process.env.accessTokenPublicKey as string;
+    return jwt.verify(token, publicKey) as T;
+  } catch (error) {
+    return null;
+  }
+};
