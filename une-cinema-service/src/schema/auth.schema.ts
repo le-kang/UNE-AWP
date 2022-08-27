aa212f81d2f4473ec4ef49641564591abe5d9fee
Yihan Lu <yihanlu@Yihans-iMac.localdomain>
2022-08-27 14:38:22 +1000

    add schema and service

---
+++ une-cinema-service/src/schema/auth.schema.ts
@@ -0,0 +1,23 @@
+import { object, string, number, array, TypeOf, union } from "zod";
+
+const payload = {
+  body: object({
+    username: string({
+      required_error: "Username is required",
+    }),
+    password: string({
+      required_error: "Password is required",
+    }),
+  })
+};
+
+export const registerSchema = object({
+  ...payload
+})
+
+export const loginSchema = object({
+  ...payload
+})
+
+export type RegisterInput = TypeOf<typeof registerSchema>;
+export type LoginInput = TypeOf<typeof loginSchema>;
---
+++ une-cinema-service/src/service/auth.service.ts
@@ -0,0 +1,14 @@
+import mongoose, { DocumentDefinition } from 'mongoose';
+import UserModel, { UserDocument } from '../model/user.model';
+
+export async function getUserByUsername(username: string) {
+  return UserModel.findOne({ username }).lean();
+}
+
+export async function getUserById(id: string) {
+  return UserModel.findOne({_id: new mongoose.Types.ObjectId(id)}).lean();
+}
+
+export async function createUser(user: DocumentDefinition<UserDocument>) {
+  return UserModel.create(user);
+}
