ac7542e120b9f06d32e0f594e0482f2024ed4850
Le Kang <le.kang@lendi.com.au>
2022-09-02 13:04:54 +1000

    Update the bookings page

--- une-cinema-react/src/pages/Bookings.tsx
+++ une-cinema-react/src/pages/Bookings.tsx
@@ -1,45 +1,56 @@
-import { useContext } from 'react'
+import { useContext, useCallback, useState, useEffect } from 'react'
 import { Navigate, useNavigate } from 'react-router-dom'
 import { UserContext } from '../context'
-import { useLocalStorage } from '../hooks'
-import movies from '../data/movies.json'
-import sessions from '../data/sessions.json'
+import { get } from '../utils/http'
+import { Booking } from '../types'
+
 import style from './Bookings.module.css'
 
 export default function Bookings() {
-  const { user } = useContext(UserContext)
+  const { user, logout } = useContext(UserContext)
   const navigate = useNavigate()
-  const [bookings] = useLocalStorage<Record<string, number[]>>('bookings', {})
+  const [bookings, setBookings] = useState<Booking[]>([])
+
+  const fetchBookings = useCallback(async () => {
+    try {
+      const result = await get<Booking[]>('/api/bookings')
+      setBookings(result)
+    } catch (error) {
+      console.log((error as Error).message)
+      logout()
+      navigate('/')
+    }
+  }, [logout, navigate])
+
+  useEffect(() => {
+    if (!user) return
+    fetchBookings()
+  }, [fetchBookings, user])
+
   if (!user) return <Navigate to="/login" />
   return (
     <div className={style.container}>
-      <h1 className={style.header}>
-        You have {Object.keys(bookings).length} bookings
-      </h1>
-      {Object.keys(bookings).map((key) => {
-        const sessionId = key.split('-')[1]
-        const session = sessions.find((s) => s.id === parseInt(sessionId))
-        if (!session) return null
-        const seatsNumber = bookings[key].length
-        if (seatsNumber === 0) return null
-        const movieTitle = movies.find((m) => m.id === session.movieId)?.title
-        return (
-          <div className={style.list} key={key}>
-            <p className={style.title}>
-              {movieTitle} @{session.time}
-            </p>
-            <p>
-              {seatsNumber} {seatsNumber > 1 ? 'seats' : 'seat'} booked
-            </p>
-            <button
-              className={style.button}
-              onClick={() => navigate(`/session/${sessionId}`)}
-            >
-              View/Edit
-            </button>
-          </div>
-        )
-      })}
+      <h1 className={style.header}>You have {bookings.length} bookings</h1>
+      {bookings.map(
+        ({ _id, sessionId, sessionTime, seatCount, movieTitle }) => {
+          return (
+            <div className={style.list} key={_id}>
+              <p className={style.title}>
+                {movieTitle} @{sessionTime}
+              </p>
+              <p>
+                {seatCount} {seatCount > 1 ? 'seats' : 'seat'} booked
+              </p>
+              <button
+                className={style.button}
+                onClick={() => navigate(`/session/${sessionId}`)}
+              >
+                View/Edit
+              </button>
+            </div>
+          )
+        }
+      )}
     </div>
   )
 }
